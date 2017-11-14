a1ec6ed0c76c28fad95ffff0d99703346fddfcfa
Murali Rao <mdrao.g@gmail.com>
2017-11-14 12:41:27 +0530

    Update app.py

--- app.py
+++ app.py
@@ -4,7 +4,10 @@ from chatterbot.trainers import ChatterBotCorpusTrainer, ListTrainer
 
 app = Flask(__name__)
 
+#Chatbot object creation
 english_bot = ChatBot("Rao")
+
+#Custom conversation list
 conversation = [
     "Hello",
     "Hi there!",
@@ -19,18 +22,21 @@ conversation = [
     "lets explain about the gold plan.. are you clear about this?"
 ]
 
+#set the type of trainer to chatbot
 english_bot.set_trainer(ListTrainer)
+#set the list for train
 english_bot.train(conversation)
 
-
+#Flask root
 @app.route("/")
 def home():
     return render_template("chat_window.html")
 
+#Flask plan GET end point
 @app.route("/get/<string:query>")
 def get_raw_response(query):
     return str(english_bot.get_response(query))
 
-
+#Application running port
 if __name__ == "__main__":
     app.run(port=5002)
